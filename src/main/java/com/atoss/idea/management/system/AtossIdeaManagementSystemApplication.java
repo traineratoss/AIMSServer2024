f838787dca57bd517d4e366eb904abcfc9b11b3d
traineratoss2 <traineratoss2@gmail.com>
2023-07-05 10:45:33 +0300

    initial commit

---
+++ src/main/java/com/atoss/idea/management/system/AtossIdeaManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.atoss.idea.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtossIdeaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtossIdeaManagementSystemApplication.class, args);
+	}
+
+}
