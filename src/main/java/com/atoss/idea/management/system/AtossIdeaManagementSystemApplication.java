bd302006c1b3bfdda9e6ffe5573d7e24b22a6a4a
traineratoss2 <traineratoss2@gmail.com>
2023-07-05 13:42:48 +0300

    Added maven site plugin. Solved checkstyle. Adapted the indentation checkstyle to match the Eclipse formatting.

--- src/main/java/com/atoss/idea/management/system/AtossIdeaManagementSystemApplication.java
+++ src/main/java/com/atoss/idea/management/system/AtossIdeaManagementSystemApplication.java
@@ -3,6 +3,11 @@ package com.atoss.idea.management.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class for the Spring Boot application.
+ *
+ * @author traineratoss2
+ */
 @SpringBootApplication
 public class AtossIdeaManagementSystemApplication {
 
