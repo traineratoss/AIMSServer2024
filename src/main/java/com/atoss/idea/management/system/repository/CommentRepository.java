bbe6b96708acd67a773b8fbb496c7bf4faae9dd1
FlorinCP <florincosmin.peana@ulbsibiu.ro>
2023-07-18 15:07:17 +0300

    changed some imports from * to ...

--- src/main/java/com/atoss/idea/management/system/repository/CommentRepository.java
+++ src/main/java/com/atoss/idea/management/system/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.atoss.idea.management.system.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CommentRepository {
 }
--- src/main/java/com/atoss/idea/management/system/repository/entity/Comment.java
+++ src/main/java/com/atoss/idea/management/system/repository/entity/Comment.java
@@ -5,12 +5,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
-
 
 @ToString
 @Component
