ebdb83496c4ccb2058dbd0b70238137a589bc2ee
FlorinCP <florincosmin.peana@ulbsibiu.ro>
2023-07-18 14:56:33 +0300

    added comment fields

--- src/main/java/com/atoss/idea/management/system/repository/entity/Comment.java
+++ src/main/java/com/atoss/idea/management/system/repository/entity/Comment.java
@@ -1,14 +1,43 @@
 package com.atoss.idea.management.system.repository.entity;
 
 
-import lombok.Data;
-import lombok.ToString;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
 
 @ToString
 @Component
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Comment {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "idea_id")
+    private Long ideaId;
+
+    @Column(name = "parent_id")
+    private Long parentId;
+
+    @Column(name = "comment_text")
+    private String commentText;
+
+    @Column(name = "creation_date")
+    private Date creationDate;
 
 }
--- src/main/java/com/atoss/idea/management/system/service/CommentService.java
+++ src/main/java/com/atoss/idea/management/system/service/CommentService.java
@@ -1,4 +1,12 @@
 package com.atoss.idea.management.system.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public interface CommentService {
+
+
+
+
 }
