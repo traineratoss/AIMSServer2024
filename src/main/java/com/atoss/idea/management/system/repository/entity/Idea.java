9de09269b4328575b65729d0b4234dfc92733903
rarestrifu <rarestrifu2001@yahoo.com>
2023-07-18 15:58:29 +0300

    updated idead and category

--- src/main/java/com/atoss/idea/management/system/repository/entity/Idea.java
+++ src/main/java/com/atoss/idea/management/system/repository/entity/Idea.java
@@ -1,4 +1,66 @@
 package com.atoss.idea.management.system.repository.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Table(name = "idea")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Idea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idea_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "text")
+    private String text;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "image_id", referencedColumnName = "id")
+    @Column(name = "image")
+    private Image image;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "idea_category",
+            joinColumns = { @JoinColumn(name = "idea_id") },
+            inverseJoinColumns = { @JoinColumn(name = "category_id") }
+    )
+    @Column(name = "categoryList")
+    private List<Category> categoryList;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @Column(name = "user")
+    private User user;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "idea", cascade = CascadeType.ALL)
+    @Column(name = "comment_list")
+    private List<Comment> commentList;
 }
--- src/main/java/com/atoss/idea/management/system/repository/entity/Image.java
+++ src/main/java/com/atoss/idea/management/system/repository/entity/Image.java
@@ -1,4 +1,33 @@
 package com.atoss.idea.management.system.repository.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.sql.Blob;
+
+@Entity
+@Table(name = "image")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Lob
+    private Blob image;
+
+    @OneToOne(mappedBy = "image")
+    private Idea idea;
 }
