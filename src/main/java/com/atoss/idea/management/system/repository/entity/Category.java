6013795a51aad4e6f4d1ab9ae63ca4ad53cad8a1
Chirila-Tudor <tchirila2712@gmail.com>
2023-07-18 15:52:54 +0300

    Category entity

--- src/main/java/com/atoss/idea/management/system/repository/entity/Category.java
+++ src/main/java/com/atoss/idea/management/system/repository/entity/Category.java
@@ -1,4 +1,30 @@
 package com.atoss.idea.management.system.repository.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.CascadeType;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToMany(mappedBy = "categoryList", cascade = CascadeType.ALL)
+    private List<Idea> ideaList;
+
 }
