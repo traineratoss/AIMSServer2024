f287355c4d381a7af65f6cc8bfa3aa2b061c491a
DragosBrex <dragosbrex@gmail.com>
2023-08-01 11:13:35 +0300

    #11 Replaced "findCategoryById()" with default CRUDRepository method "findById()", made necessary changes (Optional get() and isPresent()) CR: Alexandra M, Rares T

--- src/main/java/com/atoss/idea/management/system/repository/CategoryRepository.java
+++ src/main/java/com/atoss/idea/management/system/repository/CategoryRepository.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByText(String text);
 
-    Category findCategoryById(Long id);
 }
