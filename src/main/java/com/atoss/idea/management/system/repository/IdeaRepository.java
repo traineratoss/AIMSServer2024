494e0718ae89068abeecf0ce7b6e0fa1dd24fc93
rarestrifu <rarestrifu2001@yahoo.com>
2023-08-03 15:30:44 +0300

    #33  Resolved Issue : JavaDoc - IdeaRepository.java
    CR: Chirila Tudor

--- src/main/java/com/atoss/idea/management/system/repository/IdeaRepository.java
+++ src/main/java/com/atoss/idea/management/system/repository/IdeaRepository.java
@@ -1,11 +1,7 @@
 package com.atoss.idea.management.system.repository;
 
-import com.atoss.idea.management.system.repository.dto.IdeaPageDTO;
 import com.atoss.idea.management.system.repository.entity.Idea;
 import com.atoss.idea.management.system.repository.entity.Status;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,19 +9,45 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface IdeaRepository extends JpaRepository<Idea, Long> {
+
+    /**
+     * Searches in the Database the ideas who belong to a user with a
+     * given username
+     *
+     * @param username the username of the User whose ideas belong to
+     * @param pageable it contains all the necessary information about the
+     *                 requested page, such as page size, page number,
+     *                 sort category and sort direction
+     * @return returns from the DB all the ideas who belong to a user with a
+     *         given username, paged
+     */
     @Query("SELECT i FROM Idea i JOIN i.user u WHERE u.username = :username")
     Page<Idea> findAllByUserUsername(@Param("username") String username, Pageable pageable);
 
+    /**
+     * Counts in the Database the number of idea who have a specific status
+     *
+     * @param status the given Status
+     * @return the number of ideas who have this status
+     */
     Long countByStatus(Status status);
 
+    /**
+     * Counts in the Database the number of comments
+     *
+     * @return the number of comments
+     */
     @Query(value = "SELECT COUNT(parent_id) FROM comment c", nativeQuery = true)
     Long countComments();
 
-    @Query(value = "SELECT COUNT(*) FROM comment WHERE parent_id IS NULL", nativeQuery = true)
+    /**
+     * Counts in the Database the number of replies
+     *
+     * @return the number of replies
+     */
+    @Query(value = "SELECT COUNT(*) FROM comment WHERE parent_id I S NULL", nativeQuery = true)
     Long countAllReplies();
 
 }
