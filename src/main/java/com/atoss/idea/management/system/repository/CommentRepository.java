8ae913a6ef7b886edbc1c50a66d7f7c3d71b21df
alexandramoise <alexandramoise952@gmail.com>
2023-08-03 15:25:27 +0300

    #19 added necessary comments for javadoc for CommentRepository CR: Dragos B

--- src/main/java/com/atoss/idea/management/system/repository/CommentRepository.java
+++ src/main/java/com/atoss/idea/management/system/repository/CommentRepository.java
@@ -1,7 +1,5 @@
 package com.atoss.idea.management.system.repository;
 
-import com.atoss.idea.management.system.repository.dto.ResponseCommentDTO;
-import com.atoss.idea.management.system.repository.dto.ResponseCommentReplyDTO;
 import com.atoss.idea.management.system.repository.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +8,25 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    /**
+     * Overwriting default "findAllById" CRUD method in order to return a Page of type Comment
+     *
+     * Method returns a Page of comments that belong to a certain idea
+     *
+     * @param ideaId the unique identifier for the idea we are working with
+     * @param pageable the Pageable object containing pagination information
+     * @return Page of type Comment with the comments of the desired idea
+     */
     Page<Comment> findAllByIdeaId(Long ideaId, Pageable pageable);
 
+    /**
+     * Overwriting default "findAllById" CRUD method in order to return a Page of type Comment
+     *
+     * Method returns a Page of replies that belong to a certain comment
+     *
+     * @param parentId the unique identifier for the comment we are working with
+     * @param pageable the Pageable object containing pagination information
+     * @return Page of type Comment with the replies of the comment
+     */
     Page<Comment> findAllByParentId(Long parentId, Pageable pageable);
 }
