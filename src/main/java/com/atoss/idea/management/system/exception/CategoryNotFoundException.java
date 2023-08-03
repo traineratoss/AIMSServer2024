31f1b1cb1a33a28f79b37a0fd42333bd57c1b194
IuliaDragoiuGeorgiana <iuliadragoiu2@gmail.com>
2023-08-03 16:25:28 +0300

    #28 JavaDoc-CategoryNotFoundException
    cr: RaresT

--- src/main/java/com/atoss/idea/management/system/exception/CategoryNotFoundException.java
+++ src/main/java/com/atoss/idea/management/system/exception/CategoryNotFoundException.java
@@ -2,10 +2,18 @@ package com.atoss.idea.management.system.exception;
 
 public class CategoryNotFoundException extends RuntimeException {
 
+    /**
+     *  Default constructor.
+     */
     public CategoryNotFoundException() {
 
     }
 
+    /**
+     *   Constructor with a specified detail message.
+     *
+     * @param message The detail message
+     */
     public CategoryNotFoundException(String message) {
         super(message);
     }
