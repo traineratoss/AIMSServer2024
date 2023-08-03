1b89cd2bd792ef07ef8d2ce7a1514716da9f64b2
Chirila Tudor Octavian <124706013+Chirila-Tudor@users.noreply.github.com>
2023-08-03 15:34:51 +0300

    #20 added necessary comments for JavaDoc #16 modified the description of the addImage method CR: Dragoiu Iulia

--- src/main/java/com/atoss/idea/management/system/controller/ImageController.java
+++ src/main/java/com/atoss/idea/management/system/controller/ImageController.java
@@ -2,7 +2,7 @@ package com.atoss.idea.management.system.controller;
 
 import com.atoss.idea.management.system.exception.ImageNotFoundException;
 import com.atoss.idea.management.system.repository.dto.ImageDTO;
-import com.atoss.idea.management.system.service.implementation.ImageServiceImpl;
+import com.atoss.idea.management.system.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
@@ -17,24 +17,48 @@ import java.util.List;
 @RequestMapping("/aims/api/v1/images")
 
 public class ImageController {
-    private final ImageServiceImpl imageService;
+    private final ImageService imageService;
 
+    /**
+     * Constructor
+     *
+     * @param imageService Dependency injection through constructor
+     */
     @Autowired
-     public ImageController(ImageServiceImpl imageService) {
+     public ImageController(ImageService imageService) {
         this.imageService = imageService;
     }
 
+    /**
+     * Uploads an image to the database in the form of an ImageDTO object.
+     *
+     * @param file the multipart file representing the image to be uploaded.
+     * @return it returns an ImageDTO that represents the added image.
+     * @throws IOException it throws when the I/O operation fails, or it was interrupted.
+     */
     @PostMapping("/addImage")
     public ImageDTO addImage(@RequestBody MultipartFile file) throws IOException {
         return imageService.addImage(file);
-
     }
 
+    /**
+     * Gets all the images from the database.
+     *
+     * @return it returns all the images from the database.
+     */
     @GetMapping
     public List<ImageDTO> getAllImages() {
         return imageService.getAllImage();
     }
 
+    /**
+     * Gets a image from the database by the provided image id
+     *
+     * @param id it is for getting the image by id.
+     * @return it returns the selected image by the id of the image.
+     * @throws ImageNotFoundException if the image we want to get doesn't exist into the database
+     *                                it will throw an exception
+     */
     @GetMapping("/{id}")
     public ImageDTO getImage(@PathVariable("id") Long id) throws ImageNotFoundException {
         return imageService.getImage(id);
--- src/main/java/com/atoss/idea/management/system/service/ImageService.java
+++ src/main/java/com/atoss/idea/management/system/service/ImageService.java
@@ -9,28 +9,28 @@ import java.util.List;
 public interface ImageService {
 
     /**
-     * Creates an image that is selected by the user
+     * This method will save the image that is received as a parameter in the database.
      *
-     * @param imageFile it is for the image we receive from the client
-     * @return it returns an ImageDTO that represents the added image data
-     * @throws IOException it throws when the I/O operation fails, or it was interrupted
+     * @param imageFile it is for the image we receive from the client.
+     * @return it returns an ImageDTO that represents the added image data.
+     * @throws IOException it throws when the I/O operation fails, or it was interrupted.
      */
     ImageDTO addImage(MultipartFile imageFile) throws IOException;
 
     /**
-     * Gets the image that is requested by the id of the image
+     * Gets the image that is requested by the id of the image.
      *
-     * @param id it is for getting the image by id
-     * @return it returns an ImageDto
+     * @param id it is for getting the image by id.
+     * @return it returns an ImageDto.
      * @throws ImageNotFoundException if the image we want to get doesn't exist into the database
-     *                                it will throw an exception
+     *                                it will throw an exception.
      */
      ImageDTO getImage(Long id) throws ImageNotFoundException;
 
     /**
-     * Gets all the images from the database
+     * Gets all the images from the database.
      *
-     * @return it returns a list of images
+     * @return it returns a list of images.
      */
     List<ImageDTO> getAllImage();
 
