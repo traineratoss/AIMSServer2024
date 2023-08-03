cb1f0678d9d7bedcd89da4fb6658ca02faeb016a
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-08-03 15:40:02 +0300

    #24 Add javadoc for UserController CR: Mihai

--- src/main/java/com/atoss/idea/management/system/controller/UserController.java
+++ src/main/java/com/atoss/idea/management/system/controller/UserController.java
@@ -5,10 +5,13 @@ import com.atoss.idea.management.system.service.UserService;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.atoss.idea.management.system.exception.UserAlreadyExistException;
+import com.atoss.idea.management.system.exception.UserNotFoundException;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -16,10 +19,29 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    /**
+     * Constructs a new instance of the UserController with the provided UserService.
+     *
+     * @param userService The UserService instance to be used by the UserController.
+     *
+     * @see UserService
+     */
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * Adds a new user to the database.
+     *
+     * @param username The unique parameter representing the username of the new user.
+     * @param email    The unique parameter representing the email address of the new user.
+     * @return A ResponseEntity with the UserResponseDTO containing information about the newly added user.
+     * @throws UserAlreadyExistException If the provided username or email is already used by another user in the database.
+     *
+     * @see UserResponseDTO
+     * @see UserAlreadyExistException
+     * @see UserService#addUser(String, String)
+     */
     @Transactional
     @PostMapping
     public ResponseEntity<UserResponseDTO> addUser(@RequestParam(name = "username") String username,
@@ -27,29 +49,76 @@ public class UserController {
         return new ResponseEntity<>(userService.addUser(username, email), HttpStatus.CREATED);
     }
 
+    /**
+     * Updates the user's profile based on the provided username and UserUpdateDTO.
+     *
+     * @param username      The unique username of the user whose profile needs to be updated.
+     * @param userUpdateDTO The UserUpdateDTO object containing the updated user information.
+     * @return A UserResponseDTO object representing the updated user's profile information.
+     *
+     * @see UserUpdateDTO
+     * @see UserResponseDTO
+     * @see UserService#updateUserByUsername(String, UserUpdateDTO)
+     */
     @Transactional
     @PatchMapping("/update-profile")
     public UserResponseDTO updateUserByUsername(@RequestParam(value = "username") String username, @RequestBody UserUpdateDTO userUpdateDTO) {
         return userService.updateUserByUsername(username, userUpdateDTO);
     }
 
+    /**
+     * Updates the role of a user based on the provided username.
+     *
+     * @param username The username of the user whose role needs to be updated.
+     * @return A UserResponseDTO object representing the updated user's profile information after the role update.
+     * @throws UserNotFoundException If the user with the given username is not found in the system.
+     *
+     * @see UserResponseDTO
+     * @see UserNotFoundException
+     * @see UserService#updateUserRole(String)
+     */
     @Transactional
     @PatchMapping("/update-role")
     public UserResponseDTO updateUserRole(@RequestParam(value = "username") String username) {
         return userService.updateUserRole(username);
     }
 
+    /**
+     * Retrieves the user's profile information based on the provided username.
+     *
+     * @param username The username of the user whose profile information needs to be retrieved.
+     * @return A UserResponseDTO object representing the user's profile information.
+     * @throws UserNotFoundException If the user with the given username is not found in the system.
+     *
+     * @see UserResponseDTO
+     * @see UserNotFoundException
+     * @see UserService#getUserByUsername(String)
+     */
     @Transactional
     @GetMapping
     public UserResponseDTO getUserByUsername(@RequestParam(name = "username") String username) {
         return userService.getUserByUsername(username);
     }
 
+    /**
+     * Retrieves a paginated list of all users for the admin panel.
+     *
+     * @param pageSize     The number of users to be included in each page of the result.
+     * @param pageNumber   The page number of the result set to retrieve.
+     * @param sortCategory The field by which the users should be sorted. It can be any valid property of the User entity.
+     * @return A ResponseEntity containing the UserPageDTO object representing a paginated list of users.
+     * @throws IllegalArgumentException If pageSize or pageNumber is less than 1.
+     *
+     * @see UserPageDTO
+     * @see PageRequest
+     * @see Sort
+     * @see UserService#getAllUsersForAdmin(Pageable)
+     */
     @Transactional
     @GetMapping("/allUsers")
     public  ResponseEntity<UserPageDTO> getAllUsersForAdmin(@RequestParam(required = true) int pageSize,
-                                                                                    @RequestParam(required = true) int pageNumber,
-                                                                                    @RequestParam(required = true) String sortCategory) {
+                                                            @RequestParam(required = true) int pageNumber,
+                                                            @RequestParam(required = true) String sortCategory) {
         return new ResponseEntity<>(
                 userService.getAllUsersForAdmin(
                         PageRequest.of(
@@ -62,6 +131,20 @@ public class UserController {
         );
     }
 
+    /**
+     * Retrieves a paginated list of all users.
+     *
+     * @param pageSize     The number of users to be included in each page of the result.
+     * @param pageNumber   The page number of the result set to retrieve.
+     * @param sortCategory The field by which the users should be sorted. It can be any valid property of the User entity.
+     * @return A ResponseEntity containing a Page of UserResponseDTO objects representing a paginated list of users.
+     * @throws IllegalArgumentException If pageSize or pageNumber is less than 1.
+     *
+     * @see UserResponseDTO
+     * @see PageRequest
+     * @see Sort
+     * @see UserService#getAllUsers(Pageable)
+     */
     @Transactional
     @GetMapping("/all")
     public  ResponseEntity<Page<UserResponseDTO>> getAllUsers(@RequestParam(required = true) int pageSize,
@@ -80,6 +163,23 @@ public class UserController {
         );
     }
 
+    /**
+     * Retrieves a paginated list of users based on the provided username and additional parameters.
+     *
+     * @param pageSize        The number of users to be included in each page of the result.
+     * @param pageNumber      The page number of the result set to retrieve.
+     * @param sortCategory    The field by which the users should be sorted. It can be any valid property of the User entity.
+     * @param username        The username used as a filter to retrieve users with a specific username.
+     * @param currentUsername The username of the current user performing the request.
+     * @return A ResponseEntity containing the UserPageDTO object representing a paginated list of users with the specified username filter.
+     * @throws IllegalArgumentException        If pageSize or pageNumber is less than 1.
+     * @throws UserNotFoundException             If the user with the given username is not found in the system.
+     *
+     * @see UserPageDTO
+     * @see PageRequest
+     * @see Sort
+     * @see UserService#getAllUsersByUsernamePageable(Pageable, String, String)
+     */
     @Transactional
     @GetMapping("/allByUsername")
     public ResponseEntity<UserPageDTO> getAllUserByUsername(@RequestParam(required = true) int pageSize,
@@ -101,6 +201,21 @@ public class UserController {
         );
     }
 
+    /**
+     * Retrieves a paginated list of users based on their active status.
+     *
+     * @param isActive      The boolean flag indicating whether to retrieve active (true) or inactive (false) users.
+     * @param pageSize      The number of users to be included in each page of the result.
+     * @param pageNumber    The page number of the result set to retrieve.
+     * @param sortCategory  The field by which the users should be sorted. It can be any valid property of the User entity.
+     * @return A ResponseEntity containing a Page of UserResponseDTO objects representing a paginated list of users with the specified active status.
+     * @throws IllegalArgumentException        If pageSize or pageNumber is less than 1.
+     *
+     * @see UserResponseDTO
+     * @see PageRequest
+     * @see Sort
+     * @see UserService#getAllPendingUsers(boolean, Pageable)
+     */
     @Transactional
     @GetMapping("/allByIsActive")
     public ResponseEntity<Page<UserResponseDTO>> getAllUserByIsActive(@RequestParam(name = "isActive") boolean isActive,
@@ -120,6 +235,18 @@ public class UserController {
         );
     }
 
+    /**
+     * Changes the password for the user.
+     *
+     * @param changePasswordDTO The ChangePasswordDTO object containing the necessary information for password change.
+     * @return A ResponseEntity representing the result of the password change operation.
+     *         If the password is successfully changed, the response entity has an HTTP status of 200 (HttpStatus.OK).
+     *         If the password change operation fails, the response entity has an HTTP status of 400 (HttpStatus.BAD_REQUEST).
+     *
+     * @see ChangePasswordDTO
+     * @see ResponseEntity
+     * @see UserService#changePassword(ChangePasswordDTO)
+     */
     @Transactional
     @PostMapping("/change-password")
     public ResponseEntity<Object> changePassword(@RequestBody ChangePasswordDTO changePasswordDTO) {
