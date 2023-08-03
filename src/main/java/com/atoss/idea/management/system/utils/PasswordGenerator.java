e324f1eb23f9503d2fb6d675b6c09ae0f9d8073b
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-08-03 17:38:24 +0300

    #46 Javadoc for PasswordGenerator CR: Dontu

--- src/main/java/com/atoss/idea/management/system/utils/PasswordGenerator.java
+++ src/main/java/com/atoss/idea/management/system/utils/PasswordGenerator.java
@@ -3,6 +3,16 @@ package com.atoss.idea.management.system.utils;
 import java.security.SecureRandom;
 
 public class PasswordGenerator {
+
+    /**
+     * Generates a random password of the specified length using characters from a predefined set.
+     *
+     * @param len The length of the password to be generated.
+     *
+     * @return A randomly generated password as a string.
+     *
+     * @see SecureRandom
+     */
     public static String generatePassayPassword(int len) {
         final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         SecureRandom random = new SecureRandom();
