75af180b32f2d0259fa7de51ee9af160819d7a02
DragosBrex <dragosbrex@gmail.com>
2023-08-03 13:33:57 +0300

    #15 added necessary comments for javadoc for CommentService Interface CR: Alexandra M

--- src/main/java/com/atoss/idea/management/system/service/implementation/CommentServiceImpl.java
+++ src/main/java/com/atoss/idea/management/system/service/implementation/CommentServiceImpl.java
@@ -43,7 +43,6 @@ public class CommentServiceImpl implements CommentService {
 
     /**
      * CONSTRUCTOR
-     *
      * @param commentRepository needed
      * @param ideaRepository  needed
      * @param userRepository needed
