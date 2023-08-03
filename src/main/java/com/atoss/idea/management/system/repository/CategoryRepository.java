28ca50e5501b634dc30783d38d10d8f28449d0f8
Chirila Tudor Octavian <124706013+Chirila-Tudor@users.noreply.github.com>
2023-08-03 16:35:01 +0300

    #37 added necessary comments for JavaDoc CR: Dragoiu Iulia

--- src/main/java/com/atoss/idea/management/system/repository/CategoryRepository.java
+++ src/main/java/com/atoss/idea/management/system/repository/CategoryRepository.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    /**
+     * Finds a Category object by its associated text.
+     *
+     * @param text it's the category text that needs to be found.
+     * @return the Category object that matches the provided text.
+     */
     Category findByText(String text);
 
 }
