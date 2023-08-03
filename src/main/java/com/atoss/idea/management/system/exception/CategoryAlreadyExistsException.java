6567bf89754ac9af193d9276f928b4b412f34fe5
IuliaDragoiuGeorgiana <iuliadragoiu2@gmail.com>
2023-08-03 16:32:58 +0300

    #36 JavaDoc-CategoryAlreadyExistsException
    cr: RaresT

--- src/main/java/com/atoss/idea/management/system/exception/CategoryAlreadyExistsException.java
+++ src/main/java/com/atoss/idea/management/system/exception/CategoryAlreadyExistsException.java
@@ -1,10 +1,18 @@
 package com.atoss.idea.management.system.exception;
 
 public class CategoryAlreadyExistsException extends RuntimeException {
+    /**
+     *  Default constructor.
+     */
 
     public CategoryAlreadyExistsException() {
 
     }
+    /**
+     *   Constructor with a specified detail message.
+     *
+     * @param message The detail message
+     */
 
     public CategoryAlreadyExistsException(String message) {
         super(message);
