bd877d48314900b70746e55e6098a6ccccb1bdc6
IuliaDragoiuGeorgiana <iuliadragoiu2@gmail.com>
2023-08-03 16:41:44 +0300

    #41 JavaDoc-FieldValidationException
    cr: RaresT

--- src/main/java/com/atoss/idea/management/system/exception/FieldValidationException.java
+++ src/main/java/com/atoss/idea/management/system/exception/FieldValidationException.java
@@ -1,14 +1,31 @@
 package com.atoss.idea.management.system.exception;
 
 public class FieldValidationException extends RuntimeException {
+    /**
+     *  Default constructor.
+     */
+
 
     public FieldValidationException() {
 
     }
 
+    /**
+     *   Constructor with a specified detail message.
+     *
+     * @param message The detail message
+     */
+
     public FieldValidationException(String message) {
         super(message);
     }
+
+    /**
+     * Constructor with a specified detail message and cause
+     *
+     * @param message The detail message
+     * @param cause  The cause for witch user has an error message
+     */
 
     public FieldValidationException(String message, Throwable cause) {
         super(message, cause);
