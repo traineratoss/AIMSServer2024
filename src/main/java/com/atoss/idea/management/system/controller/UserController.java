bf1d8a682a6f6709dce3c1d8379a09322ea27148
MihaiV0 <131472508+MihaiV0@users.noreply.github.com>
2023-08-03 15:43:19 +0300

    #24 Javadoc for UserController CR: Cristian Cojocaru

--- src/main/java/com/atoss/idea/management/system/controller/UserController.java
+++ src/main/java/com/atoss/idea/management/system/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.atoss.idea.management.system.controller;
 
+import com.atoss.idea.management.system.exception.*;
 import com.atoss.idea.management.system.repository.dto.*;
 import com.atoss.idea.management.system.service.UserService;
 import jakarta.transaction.Transactional;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 import com.atoss.idea.management.system.exception.UserAlreadyExistException;
 import com.atoss.idea.management.system.exception.UserNotFoundException;
@@ -258,42 +260,93 @@ public class UserController {
         return entity;
     }
 
+    /**
+     * Sends an account activation approval email to the user
+     * @param username the username of the user to which the approve email will be sent
+     * @return ResponseEntity with the message "User approve successfully" and OK HTTP status
+     * @throws UserNotFoundException when the user was not found in the database
+     * @throws ApproveAlreadyGrantedException when the user's request was already approved
+     */
     @Transactional
     @PostMapping("/send-approve-email")
     public ResponseEntity<Object> sendApproveEmail(@RequestBody String username) {
         return userService.sendApproveEmail(username);
     }
 
+    /**
+     * Sends a decline email to the user
+     * @param username the username of the user to which the decline email will be sent
+     * @return ResponseEntity with the message "User deleted successfully" and OK HTTP status
+     * @throws UserNotFoundException when the user was not found in the database
+     * @throws ApproveAlreadyGrantedException when the user's request was already approved
+     * @throws UserStatusIsActiveException when the user has already been activated
+     */
     @Transactional
     @PostMapping("/send-decline-email")
     public ResponseEntity<Object> sendDeclineEmail(@RequestBody String username) {
         return userService.sendDeclineEmail(username);
     }
 
+    /**
+     * Method used to check if the user's credentials are in the database
+     * @param username the user's username
+     * @param hashPassword the encrypted password obtained by using bcrypt algorithm
+     * @return UserSecurityDTO with the user's credentials
+     * @throws UserAlreadyDeactivatedException if the user account is deactivated
+     * @throws BadCredentialsException if the credentials entered are not valid
+     */
     @PostMapping("/login")
     public UserSecurityDTO login(@RequestParam(name = "username") String username,
                                                          @RequestBody String hashPassword) {
         return userService.login(username, hashPassword);
     }
 
+    /**
+     * Sends an email with a newly generated password to the user
+     * @param usernameOrEmail the username or email of the user which requested a new password
+     * @return ResponseEntity with the UserResponseDTO and HTTP Status OK
+     * @throws UserNotFoundException if the user specified was not found in the database
+     */
     @Transactional
     @PostMapping("/send-forgot-password")
     public ResponseEntity<UserResponseDTO> sendForgotPassword(@RequestBody String usernameOrEmail) {
         return userService.sendForgotPassword(usernameOrEmail);
     }
 
+    /**
+     * Deletes the user
+     * @param username the username of the user which will be deleted
+     * @return ResponseEntity<Boolean> with delete result and OK HTTP status
+     * @throws UserNotFoundException if the user specified by username is not found
+     */
     @Transactional
     @DeleteMapping
     public ResponseEntity<Boolean> deleteUser(@RequestBody String username) {
         return new ResponseEntity<>(userService.deleteUser(username), HttpStatus.OK);
     }
 
+    /**
+     * Sends an email to the user whose account has been deactivated
+     * @param username the username of the user to which the email will be sent
+     * @return ResponseEntity with the "Email send" message and OK HTTP Status
+     * @throws UserNotFoundException if the user is not found in the database
+     * @throws EmailFailedException if the email is not sent
+     * @throws UserAlreadyDeactivatedException if the user has already been deactivated
+     */
     @Transactional
     @PostMapping("/send-deactivate-message")
     public ResponseEntity<Object> sendDeactivateMessage(@RequestBody String username) {
         return userService.sendDeactivateEmail(username);
     }
 
+    /**
+     * Sends an email to the user whose account has been activated
+     * @param username the username of the user to which the email will be sent
+     * @return ResponseEntity with the "Email send" message and OK HTTP Status
+     * @throws UserNotFoundException if the user is not found in the database
+     * @throws EmailFailedException if the email is not sent
+     * @throws UserAlreadyActivatedException if the user has already been activated
+     */
     @Transactional
     @PostMapping("/send-activate-message")
     public ResponseEntity<Object> sendActivateMessage(@RequestBody String username) {
