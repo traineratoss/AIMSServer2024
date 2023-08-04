3076f8f07c20cf7bc4aa293446d9b73526ae428a
IuliaDragoiuGeorgiana <iuliadragoiu2@gmail.com>
2023-08-04 10:19:12 +0300

    fix ImageDTO cr: RaresT

--- src/main/java/com/atoss/idea/management/system/repository/dto/ImageDTO.java
+++ src/main/java/com/atoss/idea/management/system/repository/dto/ImageDTO.java
@@ -2,11 +2,23 @@ package com.atoss.idea.management.system.repository.dto;
 
 import lombok.Data;
 
+import java.util.Base64;
+
+
 @Data
 public class ImageDTO {
 
     private byte[] image;
     private String fileName;
     private String fileType;
+
+    /**
+     * Method is used to convert the byte array image data to a Base64 encoded string.
+     *
+     * @return convert byte array to a Base64 encoded string
+     */
+    public String getBase64Image() {
+        return Base64.getEncoder().encodeToString(image);
+    }
 
 }
