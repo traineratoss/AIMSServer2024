39b82a99426f477a84bef8acef60897f44bc01e5
gugfernanda <gugfernanda@gmail.com>
2024-07-02 13:13:01 +0300

    added spaces for checking credentials

--- src/main/java/com/atoss/idea/management/system/controller/AvatarController.java
+++ src/main/java/com/atoss/idea/management/system/controller/AvatarController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
-
+    
 @RestController  
 @RequestMapping("aims/api/v1/avatars")
 public class AvatarController {
