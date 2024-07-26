28d6f41251729ebedbf3d6ad7eaf163b6b288b4b
sularbogdan <sularbogdan@yahoo.com>
2024-07-26 09:20:53 +0300

    Update CorsConfig cr: Marius Murariu

---
+++ src/main/java/com/atoss/idea/management/system/repository/security/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.atoss.idea.management.system.repository.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigin;
+
+    @Value("${cors.allowedMethods}")
+    private String allowedMethod;
+
+    @Value("${cors.allowedHeaders}")
+    private String allowedHeaders;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(allowedOrigin);
+        config.addAllowedHeader(allowedHeaders);
+        config.addAllowedMethod(allowedMethod);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
