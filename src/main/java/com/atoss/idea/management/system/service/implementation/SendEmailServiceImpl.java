4e5921aac201f6b377915d87b1c846fc73c325d1
osnatan <natanoltman72@gmail.com>
2024-07-30 14:18:17 +0300

    refactoring

--- src/main/java/com/atoss/idea/management/system/service/implementation/SendEmailServiceImpl.java
+++ src/main/java/com/atoss/idea/management/system/service/implementation/SendEmailServiceImpl.java
@@ -122,7 +122,7 @@ public class SendEmailServiceImpl implements SendEmailService {
         Idea idea = ideaRepository.findById(ideaId).get();
         for (User user : usernames) {
             String username = user.getUsername();
-<<<<<<< Updated upstream
+
             sendEmailIdeaSubscription(
                     "title-change-subscription-template.ftl",
                     username,
@@ -131,10 +131,10 @@ public class SendEmailServiceImpl implements SendEmailService {
                     ideaId,
                     "Idea title change"
             );
-=======
+
             sendEmailIdeaSubscription("title-change-subscription-template.ftl", username, idea.getTitle(),
                     idea.getText(), ideaId, "Idea title change");
->>>>>>> Stashed changes
+
         }
     }
 
