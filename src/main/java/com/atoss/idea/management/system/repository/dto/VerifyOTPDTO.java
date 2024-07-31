888e453f2e1ab414949a9fa0f014d3f6fd63adca
MurariuMarius <mam.murariu@gmail.com>
2024-07-31 11:14:09 +0300

    Fixed naming cr: Bogdan Sular

--- src/main/java/com/atoss/idea/management/system/repository/dto/VerifyOTPDTO.java
+++
@@ -1,9 +0,0 @@
-package com.atoss.idea.management.system.repository.dto;
-
-import lombok.Data;
-
-@Data
-public class VerifyOTPDTO {
-    private String usernameOrEmail;
-    private String otpCode;
-}
